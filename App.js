bdc0718b1229a4d9c0eb732fd142fccd26a81f0f
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-05-18 13:38:20 +0200

    render flatlist and some styling

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
 
 import Colors from './constants/Colors';
+import DATA from './models/courseData';
+
+// Flatslist item component
+const Item = ({ title }) => (
+   <View style={styles.textContainer}>
+      <Text style={styles.title}>{title}</Text>
+   </View>
+);
 
 export default function App() {
+
+
    return (
       <View style={styles.container}>
-         <View style={styles.textContainer}>
-            <Text style={styles.hello} >Hello world!</Text>
-         </View>
          <StatusBar style="dark" />
+         <SafeAreaView style={styles.safeArea}>
+            <View style={styles.textContainer}>
+               <Text style={styles.hello}>Hello world!</Text>
+            </View>
+            <FlatList
+               data={DATA}
+               renderItem={({ item }) => <Item title={item.title} />}
+               keyExtractor={item => item.id}
+            />
+         </SafeAreaView>
       </View>
    );
 }
@@ -20,8 +37,10 @@ const styles = StyleSheet.create({
       backgroundColor: Colors.secondary500,
       alignItems: 'center',
       justifyContent: 'center',
+      marginTop: StatusBar.currentHeight || 0
    },
    textContainer: {
+      alignItems: 'center',
       color: 'white',
       backgroundColor: Colors.primary500,
       padding: 15,
@@ -29,10 +48,15 @@ const styles = StyleSheet.create({
       elevation: 30,
       shadowOffset: { width: 0, height: 2 },
       shadowColor: 'black',
-      shadowOpacity: 0.3
+      shadowOpacity: 0.3,
+      marginVertical: 8,
+      marginHorizontal: 16,
    },
    hello: {
       color: 'white'
-
+   },
+   title: {
+      fontSize: 32,
+      color: 'white'
    }
 });
