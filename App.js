7e37cbcb3b7e4309ebda3c8f48b2a777f486dcdc
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-05-18 13:03:37 +0200

    Added constant colors, playing with styling

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Colors from './constants/Colors';
+
 export default function App() {
    return (
       <View style={styles.container}>
-         <Text>Hello world!</Text>
-         <StatusBar style="auto" />
+         <View style={styles.textContainer}>
+            <Text style={styles.hello} >Hello world!</Text>
+         </View>
+         <StatusBar style="dark" />
       </View>
    );
 }
@@ -13,8 +17,22 @@ export default function App() {
 const styles = StyleSheet.create({
    container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: Colors.secondary500,
       alignItems: 'center',
       justifyContent: 'center',
    },
+   textContainer: {
+      color: 'white',
+      backgroundColor: Colors.primary500,
+      padding: 15,
+      borderRadius: 8,
+      elevation: 30,
+      shadowOffset: { width: 0, height: 2 },
+      shadowColor: 'black',
+      shadowOpacity: 0.3
+   },
+   hello: {
+      color: 'white'
+
+   }
 });
---
+++ constants/Colors.js
@@ -0,0 +1,4 @@
+export default Colors = {
+   primary500: '#002c62',
+   secondary500: '#afe6ff'
+}
