ccc3cf8642539b7a018d91e465b588f976b36c64
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-05-29 18:02:00 +0200

    video obj to data

--- data/courseData.js
+++ data/courseData.js
@@ -34,7 +34,28 @@ export default DATA = [
       title: 'High maintenance: How to care for your sensitive plants',
       image: require('../assets/sensitive-plants.jpg'),
       contents: ['lorem ipsum', 'dolor si amet', 'consectetur adipiscing elit', 'fusce ornare fusce quam tortor'],
-      description: 'Lorem ipsum dolor sit amet. Eos quae veniam aut iusto odio vel voluptas repellat. Qui architecto dolor ab commodi deleniti et vitae nihil.t repellat tenetur eum internos voluptatum sed nulla veniam a reprehenderit voluptatem est maxime dolorem ut ratione quas.Nam doloribus placeat quo voluptatem culpa ut veniam natus est quam velit. Et voluptatem reiciendis in Quis quos ab reprehenderit rerum non distinctio voluptatem aut ducimus accusamus ea quasi illo? 33 galisum dolore est laboriosam libero eos quia commodi ut blanditiis delectus'
+      description: 'Lorem ipsum dolor sit amet. Eos quae veniam aut iusto odio vel voluptas repellat. Qui architecto dolor ab commodi deleniti et vitae nihil.t repellat tenetur eum internos voluptatum sed nulla veniam a reprehenderit voluptatem est maxime dolorem ut ratione quas.Nam doloribus placeat quo voluptatem culpa ut veniam natus est quam velit. Et voluptatem reiciendis in Quis quos ab reprehenderit rerum non distinctio voluptatem aut ducimus accusamus ea quasi illo? 33 galisum dolore est laboriosam libero eos quia commodi ut blanditiis delectus',
+      videos: [{
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      {
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      {
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      {
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      ]
    },
    {
       id: '3',
@@ -42,7 +63,28 @@ export default DATA = [
       title: 'How to grow your plants just the way you want',
       image: require('../assets/watering-plants.jpg'),
       contents: ['lorem ipsum', 'dolor si amet', 'consectetur adipiscing elit', 'fusce ornare fusce quam tortor'],
-      description: 'Lorem ipsum dolor sit amet. Eos quae veniam aut iusto odio vel voluptas repellat. Qui architecto dolor ab commodi deleniti et vitae nihil.t repellat tenetur eum internos voluptatum sed nulla veniam a reprehenderit voluptatem est maxime dolorem ut ratione quas.Nam doloribus placeat quo voluptatem culpa ut veniam natus est quam velit. Et voluptatem reiciendis in Quis quos ab reprehenderit rerum non distinctio voluptatem aut ducimus accusamus ea quasi illo? 33 galisum dolore est laboriosam libero eos quia commodi ut blanditiis delectus'
+      description: 'Lorem ipsum dolor sit amet. Eos quae veniam aut iusto odio vel voluptas repellat. Qui architecto dolor ab commodi deleniti et vitae nihil.t repellat tenetur eum internos voluptatum sed nulla veniam a reprehenderit voluptatem est maxime dolorem ut ratione quas.Nam doloribus placeat quo voluptatem culpa ut veniam natus est quam velit. Et voluptatem reiciendis in Quis quos ab reprehenderit rerum non distinctio voluptatem aut ducimus accusamus ea quasi illo? 33 galisum dolore est laboriosam libero eos quia commodi ut blanditiis delectus',
+      videos: [{
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      {
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      {
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      {
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      ]
    },
    {
       id: '4',
@@ -50,7 +92,28 @@ export default DATA = [
       title: 'Plant Design: Decororating your house with plants',
       image: require('../assets/styling-with-plants.jpg'),
       contents: ['lorem ipsum', 'dolor si amet', 'consectetur adipiscing elit', 'fusce ornare fusce quam tortor'],
-      description: 'Lorem ipsum dolor sit amet. Eos quae veniam aut iusto odio vel voluptas repellat. Qui architecto dolor ab commodi deleniti et vitae nihil.t repellat tenetur eum internos voluptatum sed nulla veniam a reprehenderit voluptatem est maxime dolorem ut ratione quas.Nam doloribus placeat quo voluptatem culpa ut veniam natus est quam velit. Et voluptatem reiciendis in Quis quos ab reprehenderit rerum non distinctio voluptatem aut ducimus accusamus ea quasi illo? 33 galisum dolore est laboriosam libero eos quia commodi ut blanditiis delectus'
+      description: 'Lorem ipsum dolor sit amet. Eos quae veniam aut iusto odio vel voluptas repellat. Qui architecto dolor ab commodi deleniti et vitae nihil.t repellat tenetur eum internos voluptatum sed nulla veniam a reprehenderit voluptatem est maxime dolorem ut ratione quas.Nam doloribus placeat quo voluptatem culpa ut veniam natus est quam velit. Et voluptatem reiciendis in Quis quos ab reprehenderit rerum non distinctio voluptatem aut ducimus accusamus ea quasi illo? 33 galisum dolore est laboriosam libero eos quia commodi ut blanditiis delectus',
+      videos: [{
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      {
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      {
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      {
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      ]
    },
    {
       id: '5',
@@ -58,6 +121,27 @@ export default DATA = [
       title: 'Growing guide: How to sow and grow plants from scratch',
       image: require('../assets/sprouts.jpg'),
       contents: ['lorem ipsum', 'dolor si amet', 'consectetur adipiscing elit', 'fusce ornare fusce quam tortor'],
-      description: 'Lorem ipsum dolor sit amet. Eos quae veniam aut iusto odio vel voluptas repellat. Qui architecto dolor ab commodi deleniti et vitae nihil.t repellat tenetur eum internos voluptatum sed nulla veniam a reprehenderit voluptatem est maxime dolorem ut ratione quas.Nam doloribus placeat quo voluptatem culpa ut veniam natus est quam velit. Et voluptatem reiciendis in Quis quos ab reprehenderit rerum non distinctio voluptatem aut ducimus accusamus ea quasi illo? 33 galisum dolore est laboriosam libero eos quia commodi ut blanditiis delectus'
+      description: 'Lorem ipsum dolor sit amet. Eos quae veniam aut iusto odio vel voluptas repellat. Qui architecto dolor ab commodi deleniti et vitae nihil.t repellat tenetur eum internos voluptatum sed nulla veniam a reprehenderit voluptatem est maxime dolorem ut ratione quas.Nam doloribus placeat quo voluptatem culpa ut veniam natus est quam velit. Et voluptatem reiciendis in Quis quos ab reprehenderit rerum non distinctio voluptatem aut ducimus accusamus ea quasi illo? 33 galisum dolore est laboriosam libero eos quia commodi ut blanditiis delectus',
+      videos: [{
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      {
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      {
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      {
+         url: 'url',
+         title: 'title',
+         contents: ['sldfmzlsdf', 'asfsd sdf asdfpoa fsldf', 'hej ajdf fsdssdw']
+      },
+      ]
    },
 ]
